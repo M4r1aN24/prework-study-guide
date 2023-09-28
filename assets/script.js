a9e6d7d8c01157745a03e0fda6649fab368e6f94
M4r1aN24 <142494616+M4r1aN24@users.noreply.github.com>
2023-09-28 16:12:13 +0100

    changes

--- assets/script.js
+++ assets/script.js
@@ -24,39 +24,3 @@ function helloWorld (){
 console.log("Hello, World! I am a function!");
 }
 
-listTopics()
-selectTopic()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}
-*/
-
